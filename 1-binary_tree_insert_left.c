a9e371ca720a9b5516ad5160deb441bf7e935a01
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2024-01-05 09:09:53 -0800

    task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -9,8 +9,10 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *node = NULL, *temp = NULL;
 
+	if (parent == NULL)
+		return (NULL);
 	node = malloc(sizeof(binary_tree_t));
-	if (node == NULL || parent == NULL)
+	if (node == NULL)
 		return (NULL);
 	node->n = value;
 	node->parent = parent;
