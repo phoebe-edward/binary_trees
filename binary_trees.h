ed474345623df4b886618e48fc80e3c2561af0d3
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2024-01-14 05:57:10 -0800

    task 16

--- binary_trees.h
+++ binary_trees.h
@@ -36,4 +36,6 @@ size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
 int binary_tree_is_full(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
+int my_pow(int num1, int num2);
 #endif /* _BINARY_TREES_H_ */
